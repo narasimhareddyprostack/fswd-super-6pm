4f4c4ea34ff92a75719edce72bd2c9f47fca19c7
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-11-05 04:55:29 +0530

    okie

---
+++ JS/Basics/Datatypes/Data.js
@@ -0,0 +1,17 @@
+var brand = "Peter England"; //string
+var product_Name = "Slim checks Shirts"; //string
+var price = 499; //number
+var rating = 4.5; //number
+var is_Avail = false; //boolean
+var discount; // undefined
+var size = [38, 40, 42, 44]; //object
+var specificaton = { fits: "Slim", size: "5XL" }; //object
+//how to find data type - typeof operator
+console.log(typeof brand);
+console.log(typeof product_Name);
+console.log(typeof price);
+console.log(typeof rating);
+console.log(typeof is_Avail);
+console.log(typeof discount);
+console.log(typeof size);
+console.log(typeof specificaton);
---
+++ JS/Basics/Datatypes/ExNull.js
@@ -0,0 +1,7 @@
+let delivery = null;
+console.log(delivery);
+console.log(typeof delivery);
+
+let x = 9007199254740991n;
+console.log(typeof x);
+console.log(x);
