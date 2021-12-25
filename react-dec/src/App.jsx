dda7c921120720c436e18e34ea21b27e425f42a4
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-12-25 18:57:48 +0530

    okie

---
+++ react-dec/src/App.jsx
@@ -0,0 +1,9 @@
+let App = () => {
+  return (
+    <section>
+      <h1>Hello, GM Welcome to PSA</h1>
+      <h1>Hello, GM Welcome to PSA</h1>
+    </section>
+  );
+};
+export default App;
---
+++ react-dec/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+//ReactDOM.render(1,2)
+//ReactDOM.render("Hello,GM", document.getElementById("kk"));
+//ReactDOM.render(<h1>Weolcome </h1>, document.getElementById("kk"));
+ReactDOM.render(<App />, document.getElementById("kk"));
